0f75c814331c0c93a11252d568752ea5c96a9712
LanceSicat16 <155714718+LanceSicat16@users.noreply.github.com>
2024-02-28 19:48:21 +0800

    Add files via upload

---
+++ constructor.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    uint256 public myNumber;
+    mapping(address => uint256) public valueMapping;
+
+    //Constructor function
+    constructor() {
+        myNumber = 42;
+        // Initialize a default value for the contract creator
+        valueMapping[msg.sender] = 100;
+    }
+}
